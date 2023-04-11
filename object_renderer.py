51881d83e4fb17eda215133f61cf9d39405d6ee5
nachoblwh <105123835+nachoblwh@users.noreply.github.com>
2023-04-11 19:52:59 +0200

    Add files via upload

---
+++ object_renderer.py
@@ -0,0 +1,44 @@
+import pygame as pg
+from settings import *
+
+
+class ObjectRenderer:
+    def __init__(self, game):
+        self.game = game
+        self.screen = game.screen
+        self.wall_textures = self.load_wall_textures()
+        self.sky_image = self.get_texture('resources/textures/sky.png', (WIDTH, HALF_HEIGHT))
+        self.sky_offset = 0
+        self.digit_size = 90
+        self.digit_images = [self.get_texture(f'resources/textures/digits/{i}.png', [self.digit_size] * 2)
+                             for i in range(11)]
+        self.digits = dict(zip(map(str, range(11)), self.digit_images))
+       
+
+    def draw(self):
+        self.draw_background()
+        self.render_game_objects()
+
+    def draw_background(self):
+        self.sky_offset = (self.sky_offset + 4.5 * self.game.player.rel) % WIDTH
+        self.screen.blit(self.sky_image, (-self.sky_offset, 0))
+        self.screen.blit(self.sky_image, (-self.sky_offset + WIDTH, 0))
+        # floor
+        pg.draw.rect(self.screen, FLOOR_COLOR, (0, HALF_HEIGHT, WIDTH, HEIGHT))
+
+    def render_game_objects(self):
+        list_objects = sorted(self.game.raycasting.objects_to_render, key=lambda t: t[0], reverse=True)
+        for depth, image, pos in list_objects:
+            self.screen.blit(image, pos)
+
+    @staticmethod
+    def get_texture(path, res=(TEXTURE_SIZE, TEXTURE_SIZE)):
+        texture = pg.image.load(path).convert_alpha()
+        return pg.transform.scale(texture, res)
+
+    def load_wall_textures(self):
+        return {
+            1: self.get_texture('resources/textures/1.png'),
+            2: self.get_texture('resources/textures/otrapuerta.png'),
+            3: self.get_texture('resources/textures/logojandula.jpg')
+        }
---
+++ player.py
@@ -0,0 +1,88 @@
+from settings import *
+import pygame as pg
+import math
+
+
+class Player:
+    def __init__(self, game):
+        self.game = game
+        self.x, self.y = PLAYER_POS
+        self.angle = PLAYER_ANGLE
+        self.rel = 0
+        self.time_prev = pg.time.get_ticks()
+        # diagonal movement correction
+        self.diag_move_corr = 1 / math.sqrt(2)
+
+
+    def movement(self):
+        sin_a = math.sin(self.angle)
+        cos_a = math.cos(self.angle)
+        dx, dy = 0, 0
+        speed = PLAYER_SPEED * self.game.delta_time
+        speed_sin = speed * sin_a
+        speed_cos = speed * cos_a
+
+        keys = pg.key.get_pressed()
+        num_key_pressed = -1
+        if keys[pg.K_w]:
+            num_key_pressed += 1
+            dx += speed_cos
+            dy += speed_sin
+        if keys[pg.K_s]:
+            num_key_pressed += 1
+            dx += -speed_cos
+            dy += -speed_sin
+        if keys[pg.K_a]:
+            num_key_pressed += 1
+            dx += speed_sin
+            dy += -speed_cos
+        if keys[pg.K_d]:
+            num_key_pressed += 1
+            dx += -speed_sin
+            dy += speed_cos
+
+        # diag move correction
+        if num_key_pressed:
+            dx *= self.diag_move_corr
+            dy *= self.diag_move_corr
+
+        self.check_wall_collision(dx, dy)
+
+        # if keys[pg.K_LEFT]:
+        #     self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
+        # if keys[pg.K_RIGHT]:
+        #     self.angle += PLAYER_ROT_SPEED * self.game.delta_time
+        self.angle %= math.tau
+
+
+    # COLLISIONS 
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        scale = PLAYER_SIZE_SCALE / self.game.delta_time
+        if self.check_wall(int(self.x + dx * scale), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy * scale)):
+            self.y += dy
+
+
+    def mouse_control(self):
+        mx, my = pg.mouse.get_pos()
+        if mx < MOUSE_BORDER_LEFT or mx > MOUSE_BORDER_RIGHT:
+            pg.mouse.set_pos([HALF_WIDTH, HALF_HEIGHT])
+        self.rel = pg.mouse.get_rel()[0]
+        self.rel = max(-MOUSE_MAX_REL, min(MOUSE_MAX_REL, self.rel))
+        self.angle += self.rel * MOUSE_SENSITIVITY * self.game.delta_time
+
+    def update(self):
+        self.movement()
+        self.mouse_control()
+
+    @property
+    def pos(self):
+        return self.x, self.y
+
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
