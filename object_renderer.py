989093584db5ae3119aa268da92a750d1583ecec
nachoblwh <105123835+nachoblwh@users.noreply.github.com>
2023-04-11 20:07:58 +0200

    Update object_renderer.py

--- object_renderer.py
+++ object_renderer.py
@@ -9,9 +9,6 @@ class ObjectRenderer:
         self.wall_textures = self.load_wall_textures()
         self.sky_image = self.get_texture('resources/textures/sky.png', (WIDTH, HALF_HEIGHT))
         self.sky_offset = 0
-        self.digit_size = 90
-        self.digit_images = [self.get_texture(f'resources/textures/digits/{i}.png', [self.digit_size] * 2)
-                             for i in range(11)]
         self.digits = dict(zip(map(str, range(11)), self.digit_images))
        
 
